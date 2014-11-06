746abc92333396eeedc44d0cffd76868ae48c746
theo salvador <xtian_oeht@yahoo.com>
2014-11-06 18:01:51 +0800

    Optimized JS. Fixed redirect loop in php.

--- wp-content/themes/unite-child/functions.php
+++ wp-content/themes/unite-child/functions.php
@@ -11,37 +11,39 @@ function enqueue_child_theme_styles() {
 add_action('template_redirect','send_vote',1);
 
 function send_vote(){
-	if(wp_verify_nonce( $_POST['_wpnonce'], 'votesubmit' )){
-		global $wpdb;
-		//verify if corpid exists and has not voted yet
-		$sql = "select ID from edn_corpid where corpid = '".$_POST['corpID']."'";
-		$corpID = $wpdb->get_var($sql);
-		$postID = $_POST['postID'] * 1;
-		if($corpID!=null){
-			$sql = $wpdb->prepare("select ID from edn_votes where corpid = %d",$corpID);
-			$votes = $wpdb->get_var($sql);
-			if($votes==null){
-				//insert vote
-				$columns = array('corpid'=>$corpID,
-								'postid'=>$postID,
-								'datevoted'=>date('Y-m-d'));
+	if($_POST!=null){
+		if(wp_verify_nonce( $_POST['_wpnonce'], 'votesubmit' )){
+			global $wpdb;
+			//verify if corpid exists and has not voted yet
+			$sql = "select ID from edn_corpid where corpid = '".$_POST['corpID']."'";
+			$corpID = $wpdb->get_var($sql);
+			$postID = $_POST['postID'] * 1;
+			if($corpID!=null){
+				$sql = $wpdb->prepare("select ID from edn_votes where corpid = %d",$corpID);
+				$votes = $wpdb->get_var($sql);
+				if($votes==null){
+					//insert vote
+					$columns = array('corpid'=>$corpID,
+									'postid'=>$postID,
+									'datevoted'=>date('Y-m-d'));
 
-				$wpdb->insert("edn_votes",$columns);
-				wp_redirect(add_query_arg( 'n', wp_create_nonce ('success_vote'), get_permalink($_POST['pageID']) ));
+					$wpdb->insert("edn_votes",$columns);
+					wp_redirect(add_query_arg( 'n', wp_create_nonce ('success_vote'), get_permalink($_POST['pageID']) ));
+					exit;
+				}else{
+					//error, already voted
+					wp_redirect(add_query_arg( 'n', wp_create_nonce ('duplicate_vote'), get_permalink($_POST['pageID']) ));
+					exit;
+				}
 				exit;
 			}else{
-				//error, already voted
-				wp_redirect(add_query_arg( 'n', wp_create_nonce ('duplicate_vote'), get_permalink($_POST['pageID']) ));
+				wp_redirect(add_query_arg( 'n', wp_create_nonce ('corpid_error'), get_permalink($_POST['pageID']) ));
 				exit;
 			}
-			exit;
 		}else{
-			wp_redirect(add_query_arg( 'n', wp_create_nonce ('corpid_error'), get_permalink($_POST['pageID']) ));
+			//security fail here
+			wp_redirect(add_query_arg( 'n', wp_create_nonce ('security_error'), get_permalink($_POST['pageID']) ));
 			exit;
 		}
-	}else{
-		//security fail here
-		wp_redirect(add_query_arg( 'n', wp_create_nonce ('security_error'), get_permalink($_POST['pageID']) ));
-		exit;
 	}
 }
--- wp-content/themes/unite-child/js/site.js
+++ wp-content/themes/unite-child/js/site.js
@@ -1,24 +1,25 @@
 jQuery(document).ready(function(){
+   var imgModal = jQuery('#myModal');
    jQuery('li').find('img').on('click',function(){
 		var src = jQuery(this).attr('src');
 		var title = jQuery(this).siblings('h4').text();
 		var img = '<img src="' + src + '" class="img-responsive"/>';
-		jQuery('#myModal').modal();
-		jQuery('#myModal').on('shown.bs.modal', function(){
-			jQuery('#myModal').find('.modal-body').html(img);
-			jQuery('#myModal').find('.modal-title').html(title);
-		});
-		jQuery('#myModal').on('hidden.bs.modal', function(){
-			jQuery('#myModal').find('.modal-body').html('');
-			jQuery('#myModal').find('.modal-title').html('');
-		});
+		imgModal.find('.modal-body').html(img);
+		imgModal.find('.modal-title').html(title);
+		imgModal.modal();
    }); 
+   
+	imgModal.on('hidden.bs.modal', function(){
+		jQuery('#myModal').find('.modal-body').html('');
+		jQuery('#myModal').find('.modal-title').html('');
+	});
+
 	jQuery('li').find('h4').on('click',function(){
 		jQuery(this).siblings('img').trigger('click');
 	});	
 	
 	jQuery('.vote-btn').on('click', function(e){
-		var currentTarget = $(e.currentTarget);
+		var currentTarget = jQuery(e.currentTarget);
 		jQuery('#postID').val(currentTarget.attr('rel'));
 		jQuery('#myFormModal').modal();
 		return false;
